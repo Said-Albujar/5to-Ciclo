e306fb26b7bfaccf73a42836f0a06826bc46f7b0
Said-Albujar <113253498+MIRKOXSDF@users.noreply.github.com>
2023-11-03 22:21:02 -0500

    Agarra objetos(Correcion-Avance)

--- Assets/Scripts/Players/ErrantPlayer/PickUp.cs
+++ Assets/Scripts/Players/ErrantPlayer/PickUp.cs
@@ -7,15 +7,18 @@ public class PickUp : MonoBehaviour
     public GameObject HandPoint;
     public GameObject PickedObject = null;
     private bool canPickUp;
-  
     
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.R) && PickedObject !=null)
+        if (Input.GetKeyDown(KeyCode.R) && PickedObject != null)
         {
+            this.gameObject.AddComponent<Rigidbody>().useGravity=false;
+            PickedObject.AddComponent<Rigidbody>().constraints = ~RigidbodyConstraints.FreezeRotation;
+
             PickedObject.GetComponent<Rigidbody>().useGravity = true;
-            PickedObject.GetComponent <Rigidbody>().isKinematic = false;
+            PickedObject.GetComponent<Rigidbody>().isKinematic = false;
             PickedObject.gameObject.transform.SetParent(null);
+            
             PickedObject = null;
         }
 
@@ -25,7 +28,11 @@ public class PickUp : MonoBehaviour
             PickedObject.GetComponent<Rigidbody>().useGravity = false;
             PickedObject.GetComponent<Rigidbody>().isKinematic = true;
             PickedObject.gameObject.transform.SetParent(this.gameObject.transform);
+            Destroy(this.GetComponent<Rigidbody>());
+            Destroy(PickedObject.GetComponent<Rigidbody>());
+
             PickedObject.transform.position = HandPoint.transform.position;
+
         }
 
     }
