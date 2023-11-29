4b7e34ae6b971665648df6fabf5eb70904ad3279
MIRKOXSDF <mirko06660o@gmail.com>
2023-11-29 11:11:56 -0500

    CRR_031 empujar cajas

--- Assets/Scripts/Players/ErrantPlayer/MoveBoxes.cs
+++ Assets/Scripts/Players/ErrantPlayer/MoveBoxes.cs
@@ -6,33 +6,72 @@ public class MoveBoxes : MonoBehaviour
 {
     public float radius;
     public Transform posRaycast;
+    public GameObject[] box;
     public bool inBox;
+    public float fuerzaEmpuje = 10f; // La fuerza con la que empujará el jugador
+    public LayerMask layerBox;
     //public GameObject box;
     private void Start()
     {
         
     }
+
+
+    /*void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.CompareTag("BigBox")) 
+        {
+            Rigidbody cajaRigidbody = col.gameObject.GetComponent<Rigidbody>();
+
+            if (cajaRigidbody != null)
+            {
+               
+                Vector3 direccionEmpuje = col.gameObject.transform.position - transform.position;
+
+               
+                cajaRigidbody.AddForce(direccionEmpuje.normalized * fuerzaEmpuje, ForceMode.Impulse);
+            }
+        }
+    }*/
+
     private void Update()
     {
-        RaycastHit hit;
-         inBox = Physics.Raycast(posRaycast.transform.position, posRaycast.forward, out hit, radius);
-
-        if(hit.collider.gameObject.CompareTag("BigBox"))
-        {
-            Debug.Log("col");
-            inBox = true;
-            hit.collider.gameObject.GetComponent<Rigidbody>().mass = 2;
-            hit.collider.gameObject.GetComponent<Rigidbody>().isKinematic = false;
-
-        }
-        else
-        {
-            inBox = false;
-
-            Debug.Log("noCol");
-
-        }
-
-
+        
+        inBox=Physics.Raycast(posRaycast.transform.position, posRaycast.forward, radius, layerBox);   
     }
+    private void FixedUpdate()
+    {
+
+        foreach (GameObject obj in box)
+        {
+            if (inBox)
+            {
+                Rigidbody rb2d = obj.GetComponent<Rigidbody>();
+
+                if (rb2d != null)
+                {
+
+                    Vector3 direccionEmpuje = obj.transform.position - transform.position;
+
+
+                    rb2d.AddForce(direccionEmpuje.normalized * fuerzaEmpuje * Time.fixedDeltaTime, ForceMode.Impulse);
+                }
+                obj.GetComponent<Rigidbody>().isKinematic = false;
+            }
+       
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+
+
 }
+
