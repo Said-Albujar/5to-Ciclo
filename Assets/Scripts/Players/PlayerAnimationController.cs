217a4886a3833d08d661d4be717d6457a534e349
MIRKOXSDF <mirko06660o@gmail.com>
2024-04-20 19:57:50 -0500

    Anim de saltar agarrando caja (Avance)

--- Assets/Scripts/Players/PlayerAnimationController.cs
+++ Assets/Scripts/Players/PlayerAnimationController.cs
@@ -22,6 +22,7 @@ public class PlayerAnimationController : MonoBehaviour
             anim.SetBool("isRunning", playerMovement.isRunning);
             anim.SetBool("isCrouching", playerMovement.isCrouching);
             anim.SetBool("GetUp", picked.canPickUp);
+            anim.SetBool("jumpedBox", playerMovement.isJump);
 
             MoveAnim();
             JumpAnim();
