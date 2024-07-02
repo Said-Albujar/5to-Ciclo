968b8772dd4ba7e51ecc5c8c8db7b8b1b4e8798f
bradymelendez <105991377+bradymelendez@users.noreply.github.com>
2024-07-01 21:29:35 -0500

    limpieza y orden de carpetas

--- Assets/Scripts/Menu/MainMenu.cs
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -1,49 +1,49 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.SceneManagement;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEngine.SceneManagement;
 
-public class MainMenu : MonoBehaviour
-{
-    public GameObject mainMenuPanel;
-    public Animator anim;
-    [SerializeField] private string sceneName;
-    public GameObject panelCredits;
-    // Start is called before the first frame update
-    void Start()
-    {
-        Cursor.visible = true;
-        Cursor.lockState = CursorLockMode.None;
-        panelCredits.SetActive(false);
+//public class MainMenu : MonoBehaviour
+//{
+//    public GameObject mainMenuPanel;
+//    public Animator anim;
+//    [SerializeField] private string sceneName;
+//    public GameObject panelCredits;
+//    // Start is called before the first frame update
+//    void Start()
+//    {
+//        Cursor.visible = true;
+//        Cursor.lockState = CursorLockMode.None;
+//        panelCredits.SetActive(false);
 
-    }
+//    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        Scene currentScene = SceneManager.GetActiveScene();
-        sceneName = currentScene.name;
+//    // Update is called once per frame
+//    void Update()
+//    {
+//        Scene currentScene = SceneManager.GetActiveScene();
+//        sceneName = currentScene.name;
 
-        if (Input.anyKeyDown && sceneName == "Menu")
-        {
-            anim.SetBool("pressEnter", true);
-        }
-    }
+//        if (Input.anyKeyDown && sceneName == "Menu")
+//        {
+//            anim.SetBool("pressEnter", true);
+//        }
+//    }
 
-    public void ButtonStart()
-    {
-        anim.SetBool("start", true);
-    }
-    public void ButtonCredists()
-    {
-        panelCredits.SetActive(true);
-    }
+//    public void ButtonStart()
+//    {
+//        anim.SetBool("start", true);
+//    }
+//    public void ButtonCredists()
+//    {
+//        panelCredits.SetActive(true);
+//    }
 
-    public void ButtonExitGame()
-    {
-        Application.Quit();
-        Debug.Log("Exit");
-    }
+//    public void ButtonExitGame()
+//    {
+//        Application.Quit();
+//        Debug.Log("Exit");
+//    }
 
   
-}
+//}
