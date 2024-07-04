b607d6f230f9e88292ea79581fbe7a798546637d
MIRKOXSDF <mirko06660o@gmail.com>
2024-07-04 16:18:33 -0500

    Arreglar cinematica final

--- Assets/Scripts/Menu/SceneLoad.cs
+++ Assets/Scripts/Menu/SceneLoad.cs
@@ -6,14 +6,46 @@ using UnityEngine.SceneManagement;
 public class SceneLoad : MonoBehaviour
 {
     public bool active;
+    public Material shaderDissolver;
+    public float value = 20.8f;
+    public bool activeDissolve;
+
+
     public void Start()
     {
-        if(active)
+        value = 20.8f;
+     
+        if (active)
         {
             Cursor.visible = true;
             Cursor.lockState = CursorLockMode.None;
         }
     }
+    private void Update()
+    {
+        if (shaderDissolver != null)
+        {
+            shaderDissolver.SetFloat("_dissolveAmount", value);
+            if (activeDissolve)
+            {
+                value -= 2*Time.deltaTime;
+
+            }
+        }
+      
+       
+    }
+    public void ReturnBossShader()
+    {
+        value = 20.8f;
+        activeDissolve = false;
+    }
+    public void ActiveShaderState()
+    {
+        activeDissolve = true;
+    
+    }
+ 
     public void loadScene1()
     {
         SceneManager.LoadScene("Level3");
